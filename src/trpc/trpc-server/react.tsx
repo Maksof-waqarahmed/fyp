c9d2861046b460d10528b82b7f36a14494b044b9
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2025-12-23 11:17:13 +0500

    done

--- src/trpc/trpc-server/react.tsx
+++ src/trpc/trpc-server/react.tsx
@@ -13,56 +13,73 @@ import { AppRouter } from "../api/routes";
 export const api = createTRPCReact<AppRouter>();
 
 export function TRPCReactProvider(props: {
-  children: React.ReactNode;
-  headersPromise: Promise<Headers>;
+    children: React.ReactNode;
+    headersPromise: Promise<Headers>;
 }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            refetchOnWindowFocus: true,
-            retry: (failureCount, error) => {
-              if (handleErrorOnClient(error)) return false;
-              return failureCount < 3;
-            },
-          },
-          mutations: {
-            retry: (_, error) => {
-              handleErrorOnClient(error);
-              return false;
-            },
-          },
-        },
-      }),
-  );
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        refetchOnWindowFocus: true,
+                        retry: (failureCount, error) => {
+                            if (handleErrorOnClient(error)) return false;
+                            return failureCount < 3;
+                        },
+                    },
+                    mutations: {
+                        retry: (_, error) => {
+                            handleErrorOnClient(error);
+                            return false;
+                        },
+                    },
+                },
+            }),
+    );
 
-  const [trpcClient] = useState(() =>
-    api.createClient({
-      links: [
-        // loggerLink({
-        //   enabled: (op) =>
-        //     process.env.NODE_ENV === "development" ||
-        //     (op.direction === "down" && op.result instanceof Error),
-        // }),
-        httpBatchStreamLink({
-          url: getBaseUrl() + "/api/trpc",
-          async headers() {
-            const headers = new Headers(await props.headersPromise);
-            headers.set("x-trpc-source", "nextjs-react");
-            headers.set("portal", "customer");
-            return headers;
-          },
+    const [trpcClient] = useState(() =>
+        api.createClient({
+            links: [
+                // loggerLink({
+                //   enabled: (op) =>
+                //     process.env.NODE_ENV === "development" ||
+                //     (op.direction === "down" && op.result instanceof Error),
+                // }),
+                httpBatchStreamLink({
+                    url: getBaseUrl() + "/api/trpc",
+                    async headers() {
+                        const headers = new Headers(await props.headersPromise);
+                        headers.set("x-trpc-source", "nextjs-react");
+                        headers.set("portal", "customer");
+                        return headers;
+                    },
+                }),
+            ],
         }),
-      ],
-    }),
-  );
+    );
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <api.Provider client={trpcClient} queryClient={queryClient}>
-        {props.children}
-      </api.Provider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <api.Provider client={trpcClient} queryClient={queryClient}>
+                {props.children}
+            </api.Provider>
+        </QueryClientProvider>
+    );
+}
+
+function getBaseUrl() {
+    if (typeof window !== "undefined") return window.location.origin;
+    return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+function handleErrorOnClient(error: unknown): boolean {
+    if (!(error instanceof TRPCClientError)) return false;
+    // TODO: handle authentication errors
+    if (error.data?.code === "UNAUTHORIZED") {
+        return true;
+    }
+    setTimeout(() => {
+        toast.error(error.message.slice(0, 50) + "...", { id: "error" });
+    }, 0)
+    return true;
 }
